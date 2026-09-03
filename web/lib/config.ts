c658b3a6e8314e2f7fbe36428c9d4dfd6b4dff0d
frpsantosgsf-hash <frpsantosgsf@gmail.com>
2026-09-03 15:08:31 +0200

    Add shared Sondravo platform config

---
+++ web/lib/config.ts
@@ -0,0 +1,7 @@
+export const SONDRAVO = {
+  name: 'Sondravo Family',
+  weeklyAmount: 50000,
+  payingRoleId: '1488178372700803233',
+  founderRoleId: '1488178372721901807',
+  guildId: '1488178372298408113'
+} as const;
