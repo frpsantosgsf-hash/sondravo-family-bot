2dc4e16d45cf6a1056d2c899527a816523538b48
frpsantosgsf-hash <frpsantosgsf@gmail.com>
2026-09-03 14:07:53 +0200

    Add safe bootstrap for redesigned bot

---
+++ bootstrap.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const sourcePath = path.join(__dirname, 'index.js');
+const runtimePath = path.join(__dirname, '.index-runtime.js');
+let code = fs.readFileSync(sourcePath, 'utf8');
+
+code = code.replace(
+  '[`Alle gangleden worden automatisch uit Discord gesynchroniseerd naar de tab Leden.\\",',
+  '["Alle gangleden worden automatisch uit Discord gesynchroniseerd naar de tab Leden.",'
+);
+
+fs.writeFileSync(runtimePath, code, 'utf8');
+require(runtimePath);
